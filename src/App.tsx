8e6249b922c1e8513afe90b3c1546ab43660568f
kimseokho582 <gjrehf@naver.com>
2022-01-19 17:22:45 +0900

    1-19

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgCololr};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
+  interface DummyProps {
+    text: string;
+    active?: boolean;
+  }
+
+  function Dummy({ text, active }: DummyProps) {
+    return <H1>{text}</H1>;
+  }
+
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <Dummy active={true} text="hello"></Dummy>
+    </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgCololr: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgCololr: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
